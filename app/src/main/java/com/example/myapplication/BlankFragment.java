e6ab8159b705efad0effe4a4b20712d1a921603d
Alika443 <alikarazakova7@gmail.com>
2023-02-19 15:13:19 +0300

    logic

--- app/src/main/java/com/example/myapplication/BlankFragment.java
+++ app/src/main/java/com/example/myapplication/BlankFragment.java
@@ -2,11 +2,15 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,14 +32,7 @@ public class BlankFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static BlankFragment newInstance(String param1, String param2) {
         BlankFragment fragment = new BlankFragment();
@@ -57,8 +54,35 @@ public class BlankFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)  {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        Button button1 = getView().findViewById(R.id.button1);
+        TextView textView = getView().findViewById(R.id.view);
+        Button button2 = getView().findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(R.string.button1_text);
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "Текст элемента изменён на \"" + getString(R.string.button1_text) + '"',
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(R.string.button2_text);
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "Текст элемента изменён на \"" + getString(R.string.button2_text) + '"',
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 
